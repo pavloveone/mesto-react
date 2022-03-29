7ccae8ea06ee96c39022cad213aab83fb3d9773c
Alex <pavloveone@192.168.1.6>
2022-03-29 22:30:02 +0300

    123

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-function PopupWithForm({title, name, nameButton, isOpen, onClose, children, onSubmit}) {
+function PopupWithForm({title, name, nameButton, isOpen, onClose, children, onSubmit }) {
 
   return (
     <div className={isOpen ? `popup-blackout popup_opened` : `popup-blackout`}>
       <form name={name} className='popup' onSubmit={onSubmit}>
-        <button className='popup__btn-close' onClick={onClose}></button>
+        <button type='button' className='popup__btn-close' onClick={onClose}></button>
         <h2 className='popup__title'>{title}</h2>
         <div className='popup__text-form'>
           {children}
         </div>
-        <button className='popup__btn-save'>{nameButton}</button>
+        <button type='submit' className='popup__btn-save'>{nameButton}</button>
       </form>
     </div>
   );
